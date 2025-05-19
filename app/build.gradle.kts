4f701676958f38c4ebf14d78792e553558804b6f
ender <endergosha660@yandex.ru>
2025-05-20 00:16:12 +0500

    2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
-    id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
+    id("application")
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "org.gradle.sample.Main"
+}
 
 repositories {
     mavenCentral()
